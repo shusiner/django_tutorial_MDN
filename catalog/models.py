5c7ee2f2f7ecdf30ef959ea9702f098519ca25ae
ng huan ran <quantum.plato@gmail.com>
2020-06-15 23:32:44 +0800

    tutorial 3 models addon

--- catalog/models.py
+++ catalog/models.py
@@ -11,12 +11,13 @@ class Genre(models.Model):
         """String for representing the Model object."""
         return self.name
 
-class Genre(models.Model):
-    """Model representing a book genre."""
-    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
-    
+class Language(models.Model):
+    """Model representing a Language (e.g. English, French, Japanese, etc.)"""
+    name = models.CharField(max_length=200,
+                            help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+
     def __str__(self):
-        """String for representing the Model object."""
+        """String for representing the Model object (in Admin site etc.)"""
         return self.name
 
 from django.urls import reverse # Used to generate URLs by reversing the URL patterns
@@ -43,7 +44,7 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
-        
+
 import uuid # Required for unique book instances
 
 class BookInstance(models.Model):
