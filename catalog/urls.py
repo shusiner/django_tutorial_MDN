e5f62216511ab9c7fbeb695b81d31fe52baac026
ng huan ran <quantum.plato@gmail.com>
2020-06-18 00:27:25 +0800

    tutorial 9 forms

--- catalog/urls.py
+++ catalog/urls.py
@@ -11,5 +11,19 @@ urlpatterns = [
 
 urlpatterns += [   
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-    path('borrowedbooks/', views.LoanedBooksListView.as_view(), name='borrowed'),
+    path('borrowedbooks/', views.LoanedBooksListView.as_view(), name='all-borrowed'),
+]
+
+urlpatterns += [   
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
+urlpatterns += [  
+    path('author/create/', views.AuthorCreate.as_view(), name='author_create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete'),
+]
+urlpatterns += [  
+    path('book/create/', views.BookCreate.as_view(), name='book_create'),
+    path('book/<int:pk>/update/', views.BookUpdate.as_view(), name='book_update'),
+    path('book/<int:pk>/delete/', views.BookDelete.as_view(), name='book_delete'),
 ]
