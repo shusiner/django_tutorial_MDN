b4124c604d45ea4a9078dfbb23f55b2fcd18e05f
ng huan ran <quantum.plato@gmail.com>
2020-08-07 00:54:36 +0800

    added jenkins to test

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -40,6 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_jenkins',
+]
+
+JENKINS_TASKS = [
+    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.run_pyflakes'
 ]
 
 MIDDLEWARE = [
